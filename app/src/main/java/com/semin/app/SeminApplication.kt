974180a08915caf9fce559c8aeaf7bdbeea0fb4c
osemin <semin015123@gmail.com>
2023-06-18 17:32:29 +0900

    make module

---
+++ app/src/main/java/com/semin/app/SeminApplication.kt
@@ -0,0 +1,13 @@
+package com.semin.app
+
+import android.app.Application
+
+class SeminApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+    }
+}
