67a9409e974847a820d7933bda66173a531b2238
osemin <semin015123@gmail.com>
2023-06-18 17:51:25 +0900

    add(views) : views 패키지 생성

--- presentation/src/main/java/com/semin/presentation/views/MainActivity.kt
+++ presentation/src/main/java/com/semin/presentation/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.semin.presentation
+package com.semin.presentation.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.semin.presentation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
